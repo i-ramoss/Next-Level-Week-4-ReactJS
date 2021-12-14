320c0d1e43ff4138e297301a6825c7b5ec100afe
Ian Ramos <ianramossantos@hotmail.com>
2021-12-14 13:00:34 -0300

    initial project

---
+++ moveit/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <Button color="blue" />
+      <Button color="green" />
+      <Button color="yellow" />
+    </div>
+  );
+}
+
+export default App;
