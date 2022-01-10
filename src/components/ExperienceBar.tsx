6c19fda2552ec3078c3bb4fd14a1efc2e142a5a5
Ian Ramos <ianramossantos@hotmail.com>
2022-01-10 09:54:38 -0300

    fix: change the challenge time t0 25 minutes
    fix: initial experience is 0
    
    chore: finalize application and deploy on vercel

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -11,7 +11,7 @@ export function ExperienceBar() {
 
   return (
     <header className={styles.experienceBar}>
-      <span>8 xp</span>
+      <span>0 xp</span>
       <div>
         <div style={{ width: `${percentToNextLevel}%` }}/>
 
